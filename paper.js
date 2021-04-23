e3e6ad0e34f3159bec97ce37a0db8329c43e41fa
mailtoaruna001 <73470577+mailtoaruna001@users.noreply.github.com>
2021-04-23 09:53:43 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,11 @@
+class Paper {
+    constructor(x,y){
+        var options = {
+            'density':1.2,
+            'restitution':0.3,
+            'friction':0.5,
+            'isStatic': false
+        };
+    
+    }
+}
